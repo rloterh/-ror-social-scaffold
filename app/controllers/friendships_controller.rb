c2f1ca9d73f49dc89b35013188365976a3c3b5d9
rloterh <rloterh@gmail.com>
2020-12-14 19:03:42 +0000

    Refactor Friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -12,29 +12,15 @@ class FriendshipsController < ApplicationController
     @friend = User.where('id=?', :friend_id)
   end
 
-  # GET /friendships/1
-  # GET /friendships/1.json
-  def show
-  end
-
-  # GET /friendships/new
   def new
     @friendship = Friendship.new
   end
 
-  # GET /friendships/1/edit
-  def edit
-  end
-
-  # POST /friendships
-  # POST /friendships.json
   def create
     @friendship = Friendship.create(friendship_params)
     redirect_to users_path
   end
 
-  # PATCH/PUT /friendships/1
-  # PATCH/PUT /friendships/1.json
   def update
     friendship = current_user.inverse_friendships.find_by(user_id: params[:friend_id])
     friendship.status = true
@@ -44,20 +30,16 @@ class FriendshipsController < ApplicationController
     end
   end
 
-  # DELETE /friendships/1
-  # DELETE /friendships/1.json
   def destroy
     friendship = current_user.inverse_friendships.find_by(user_id: params[:friend_id])
     redirect_to friendships_path if friendship.destroy
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_friendship
     @friendship = Friendship.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def friendship_params
     params.require(:friendship).permit(:user_id, :friend_id, :status)
   end
